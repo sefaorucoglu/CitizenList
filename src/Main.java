a6cb80ca6d9226f14af546dc69e31e9ff36603ba
onursefa0 <sefaorucoglu@gmail.com>
2022-01-14 02:06:04 +0300

    readCSV method added and CSV file added

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,64 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 public class  Main {
     public static void main(String[] args) {
-        class Connect extends SQLConfig{
-            private Connection con =null;
-            public void MakeDBConnection(){
-                String url="jdbc:mysql://" + getHost() + ":" + getPort()+ "/" + getDBİD();
+        Connect connect1 = new Connect();
+        connect1.MakeDBConnection();
+        readCSV();
 
-                try{
-                    Class.forName("com.mysql.jdbc.Driver");
-                }catch(ClassNotFoundException ex){
-                    System.out.println("Driver Not Found!");
+    }
+    static class Connect extends SQLConfig {
+        private Connection con = null;
 
-                }
-                try {
-                    con= DriverManager.getConnection(url, getUsername(),getPassword());
-                    System.out.println("DB Connection Successful");
+        public void MakeDBConnection() {
+            String url = "jdbc:mysql://" + getHost() + ":" + getPort() + "/" + getDBİD();
 
-                } catch (Exception e) {
-                    System.out.println("DB Connection Not Successful");
-
-                }
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Driver Not Found!");
 
             }
+            try {
+                con = DriverManager.getConnection(url, getUsername(), getPassword());
+                System.out.println("DB Connection Successful");
+
+            } catch (Exception e) {
+                System.out.println("DB Connection Not Successful");
+
+            }
+
         }
-        Connect connect = new Connect();
-        connect.MakeDBConnection();
+    }
+     static String[] info_array;
+    public static void readCSV() {
+         int used=1;
+         int is_used=1;
 
+        try (Scanner scanner1 = new Scanner(new FileReader("citizenList.csv"))) {
+            do {
+                String citizen_info = scanner1.nextLine();
+                info_array = citizen_info.split(",");
+            }while(is_used<used && scanner1.hasNextLine());
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            System.out.println("Somethink went wrong when file opening!");
+        }
+
+    }
+    public static void addDataToDB(){
 
 
 
     }
-    }
+
+}
 
